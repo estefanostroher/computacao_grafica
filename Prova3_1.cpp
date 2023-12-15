0ce8c0cec23787141b8ed954063f65f2372ae8a7
LucasSantosOliveira2 <lucas-santos-2011@live.com>
2023-12-15 03:12:28 -0300

    Ajustei o pino do boliche pra ficar em pé e adicionei mais dois

--- Prova3_1.cpp
+++ Prova3_1.cpp
@@ -11,11 +11,12 @@ using namespace std;
 int i = 0; // Variável global.
 float minz = 500.0;
 
-GLfloat a[] = { 0.5, 0.0, 0.5, 00 };
-GLfloat b[] = { 1.0, 1.0, 0.0, 00 };
+GLfloat a[] = {0.5, 0.0, 0.5, 00};
+GLfloat b[] = {1.0, 1.0, 0.0, 00};
 
 /* --> Construtor da classe <--*/
-Objeto::Objeto(int x, int y, int z){
+Objeto::Objeto(int x, int y, int z)
+{
     posX = x;
     posY = y;
     posZ = z;
@@ -26,11 +27,12 @@ Objeto::Objeto(int x, int y, int z){
 /* --------------------------- */
 
 /* --> Destrutor da classe <-- */
-Objeto::~Objeto(){}
+Objeto::~Objeto() {}
 /* --------------------------- */
 
 /* --> Métodos da classe <-- */
-void Objeto::carregarObjeto(string nome) {
+void Objeto::carregarObjeto(string nome)
+{
     float fx, fy, fz, a1, a2, a3;
     char letra;
 
@@ -38,106 +40,115 @@ void Objeto::carregarObjeto(string nome) {
     numFaces = 0;
 
     ifstream file;
-    file.open(nome,ios::in);
+    file.open(nome, ios::in);
 
-    if(!file) {
+    if (!file)
+    {
         cout << "\nErro ao ler o arquivo!" << endl;
         return;
     }
 
-    for(int i =0; i<0; i++)
+    for (int i = 0; i < 0; i++)
         file.ignore(INT_MAX, '\n');
 
-    while(!file.eof()) {
+    while (!file.eof())
+    {
         file >> letra;
-        if(letra == 'v') {
+        if (letra == 'v')
+        {
             file >> fx;
             file >> fy;
             file >> fz;
             cx.push_back(fx);
             cy.push_back(fy);
             cz.push_back(fz);
-            if(fz < minz)
+            if (fz < minz)
                 minz = fz;
             numVertices++;
-
         }
 
-        if(letra == 'f') {
+        if (letra == 'f')
+        {
             file >> a1;
             file >> a2;
             file >> a3;
-            v1.push_back(a1-1);
-            v2.push_back(a2-1);
-            v3.push_back(a3-1);
-            faces.push_back(face(a1-1,a2-1,a3-1));
+            v1.push_back(a1 - 1);
+            v2.push_back(a2 - 1);
+            v3.push_back(a3 - 1);
+            faces.push_back(face(a1 - 1, a2 - 1, a3 - 1));
             numFaces++;
-
         }
-    } 
-    
-    for(i = 0; i < numFaces; i++) {
-        double Px = cx[faces[i].I2]-cx[faces[i].I1];
-        double Py = cy[faces[i].I2]-cy[faces[i].I1];
-        double Pz = cz[faces[i].I2]-cz[faces[i].I1];
+    }
 
-        double Qx = cx[faces[i].I3]-cx[faces[i].I2];
-        double Qy = cy[faces[i].I3]-cy[faces[i].I2];
-        double Qz = cz[faces[i].I3]-cz[faces[i].I2];
+    for (i = 0; i < numFaces; i++)
+    {
+        double Px = cx[faces[i].I2] - cx[faces[i].I1];
+        double Py = cy[faces[i].I2] - cy[faces[i].I1];
+        double Pz = cz[faces[i].I2] - cz[faces[i].I1];
 
-        double Ci = Py*Qz - Pz*Qy;
-        double Cj = Px*Qz - Pz*Qx;
-        double Ck = Px*Qy - Py*Qx;
+        double Qx = cx[faces[i].I3] - cx[faces[i].I2];
+        double Qy = cy[faces[i].I3] - cy[faces[i].I2];
+        double Qz = cz[faces[i].I3] - cz[faces[i].I2];
 
-        double modulo = sqrt(Ci*Ci+Cj*Cj+Ck*Ck);
+        double Ci = Py * Qz - Pz * Qy;
+        double Cj = Px * Qz - Pz * Qx;
+        double Ck = Px * Qy - Py * Qx;
 
-        Ni.push_back(Ci/modulo);
-        Nj.push_back(Cj/modulo);
-        Nk.push_back(Ck/modulo);
+        double modulo = sqrt(Ci * Ci + Cj * Cj + Ck * Ck);
+
+        Ni.push_back(Ci / modulo);
+        Nj.push_back(Cj / modulo);
+        Nk.push_back(Ck / modulo);
     }
     file.close();
 }
 
-void Objeto::desenharObjeto(int textIndex){
+void Objeto::desenharObjeto(int textIndex, float escala)
+{
     int hsamples = 30, vsamples = 30;
     GLfloat l = 0.0;
     glMaterialfv(GL_FRONT, GL_AMBIENT, a);
     glMaterialfv(GL_FRONT, GL_DIFFUSE, b);
     glEnable(GL_COLOR_MATERIAL);
-	glColor4f(1.0, 1.0, 1.0, 1.0);
+    glColor4f(1.0, 1.0, 1.0, 1.0);
 
     GLfloat TexCoordX, TexCoordY;
 
-    for(i = 0; i < numFaces; i++) {
-		if(i % 2 == 1) {
-	        TexCoordX = GLfloat((i/2)%hsamples)/(float)hsamples;
-		    TexCoordY = (GLfloat)l;
+    for (i = 0; i < numFaces; i++)
+    {
+        if (i % 2 == 1)
+        {
+            TexCoordX = GLfloat((i / 2) % hsamples) / (float)hsamples;
+            TexCoordY = (GLfloat)l;
 
             glBegin(GL_POLYGON);
-            glNormal3f(Ni[i],Nj[i],Nk[i]);
-            glTexCoord2f(TexCoordX, TexCoordY+1.0/(float)vsamples);
-            glVertex3f(cx[faces[i].I1], cy[faces[i].I1], cz[faces[i].I1]);
-            glTexCoord2f(TexCoordX+1.0/(float)hsamples, TexCoordY+(1.0/(float)vsamples));
-            glVertex3f(cx[faces[i].I2], cy[faces[i].I2], cz[faces[i].I2]);
+            glNormal3f(Ni[i], Nj[i], Nk[i]);
+            glTexCoord2f(TexCoordX, TexCoordY + 1.0 / (float)vsamples);
+            glVertex3f(escala * cx[faces[i].I1], escala * cy[faces[i].I1], escala * cz[faces[i].I1]);
+            glTexCoord2f(TexCoordX + 1.0 / (float)hsamples, TexCoordY + (1.0 / (float)vsamples));
+            glVertex3f(escala * cx[faces[i].I2], escala * cy[faces[i].I2], escala * cz[faces[i].I2]);
             glTexCoord2f(TexCoordX, TexCoordY);
-            glVertex3f(cx[faces[i].I3], cy[faces[i].I3], cz[faces[i].I3]);
+            glVertex3f(escala * cx[faces[i].I3], escala * cy[faces[i].I3], escala * cz[faces[i].I3]);
             glEnd();
-		} else {
-		    glBegin (GL_POLYGON);
-            glNormal3f(Ni[i],Nj[i],Nk[i]);
+        }
+        else
+        {
+            glBegin(GL_POLYGON);
+            glNormal3f(Ni[i], Nj[i], Nk[i]);
 
-            glTexCoord2f(TexCoordX+1.0/(float)hsamples, TexCoordY+1.0/(float)vsamples);
-            glVertex3f(cx[faces[i].I1], cy[faces[i].I1], cz[faces[i].I1]);
+            glTexCoord2f(TexCoordX + 1.0 / (float)hsamples, TexCoordY + 1.0 / (float)vsamples);
+            glVertex3f(escala * cx[faces[i].I1], escala * cy[faces[i].I1], escala * cz[faces[i].I1]);
 
-            glTexCoord2f(TexCoordX+1.0/(float)hsamples, TexCoordY);
-            glVertex3f(cx[faces[i].I2], cy[faces[i].I2], cz[faces[i].I2]);
+            glTexCoord2f(TexCoordX + 1.0 / (float)hsamples, TexCoordY);
+            glVertex3f(escala * cx[faces[i].I2], escala * cy[faces[i].I2], escala * cz[faces[i].I2]);
 
             glTexCoord2f(TexCoordX, TexCoordY);
-            glVertex3f(cx[faces[i].I3], cy[faces[i].I3], cz[faces[i].I3]);
+            glVertex3f(escala * cx[faces[i].I3], escala * cy[faces[i].I3], escala * cz[faces[i].I3]);
             glEnd();
-		}
-		if(i > 0 && (i%(2*vsamples)) == 0)
-            l+=1.0/(float)vsamples;
+        }
+        if (i > 0 && (i % (2 * vsamples)) == 0)
+            l += 1.0 / (float)vsamples;
     }
 }
+
 /* ---------------------------------------------- */
--- Prova3_2.cpp
+++ Prova3_2.cpp
@@ -18,149 +18,173 @@ GLfloat light1_position[] = {0.0, 0.0, 50.0, 1.0};
 GLfloat light2_diffuse[] = {1.0, 1.0, 1.0, 1.0};
 GLfloat light2_position[] = {0.0, -50.0, -50.0, 11.0};
 GLfloat light2_lookat[] = {1.0, 0.0, 0.0, 0.0};
-GLfloat specularLight[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+GLfloat specularLight[] = {1.0f, 0.0f, 0.0f, 1.0f};
 GLfloat globalAmbient[] = {1.0, 1.0, 1.0, 1.0};
 GLfloat light2_spot[] = {0.0, 0.0, 0.0, 1.0};
-GLfloat cor_spec[] = {1.0,0.0,0.0};
-GLfloat global_ambient[] = { 0.4, 0.4, 0.4, 1 };
+GLfloat cor_spec[] = {1.0, 0.0, 0.0};
+GLfloat global_ambient[] = {0.4, 0.4, 0.4, 1};
 
 float cameraX = 0.0f, cameraY = 12.0f, cameraZ = 30.0f;
 float centerX = 0.0f, centerY = 0.0f, centerZ = 0.0f;
 float rotate = 0.0, alpha, theta, omega, sc = 1.0;
 float step_alpha = 0.2, step_gama = 0.0, raio = 1.0, gama = 0.0;
 
-vector<Objeto*> objetos;
+vector<Objeto *> objetos;
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLightfv(GL_LIGHT2, GL_SPOT_EXPONENT, light2_diffuse);
     glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
     glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, light2_lookat);
-    glLightfv(GL_LIGHT0, GL_SPECULAR,cor_spec);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, cor_spec);
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
     glLoadIdentity();
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     glEnable(GL_LIGHT2);
     glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_diffuse);
     glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
     glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, light2_spot);
-    glEnable( GL_TEXTURE_2D );
+    glEnable(GL_TEXTURE_2D);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-    gluLookAt(cameraX, cameraY,cameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    gluLookAt(cameraX, cameraY, cameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-    glScalef(sc,sc,sc);
+    glScalef(sc, sc, sc);
 
     glPushMatrix();
     glTranslatef(objetos[0]->posX, objetos[0]->posY, objetos[0]->posZ - alpha);
     glRotatef(theta, -1.0, 0.0, 0.0);
-    objetos[0]->desenharObjeto(0);
+    objetos[0]->desenharObjeto(0, 1);
     glPopMatrix();
 
     glPushMatrix();
     glTranslatef(objetos[1]->posX, objetos[1]->posY, objetos[1]->posZ - gama);
 
-    //glRotatef(theta, -1.0, 0.0, 0.0);
-    objetos[1]->desenharObjeto(1);
+    glRotatef(-90.0, 1.0, 0.0, 0.0); // Rotação para colocar o objeto em pé
+                                     // glRotatef(theta, 0.0, 0.0, 1.0);
+    objetos[1]->desenharObjeto(0, 3);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(objetos[2]->posX, objetos[2]->posY, objetos[2]->posZ - gama);
+    glRotatef(-90.0, 1.0, 0.0, 0.0);
+    objetos[2]->desenharObjeto(0, 3);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(objetos[3]->posX, objetos[3]->posY, objetos[3]->posZ - gama);
+    glRotatef(-90.0, 1.0, 0.0, 0.0);
+    objetos[3]->desenharObjeto(0, 3);
     glPopMatrix();
 
     glFlush();
     glutSwapBuffers();
 }
 
-void init() {
+void init()
+{
     glShadeModel(GL_FLAT);
     glClearDepth(100.0f);
     glEnable(GL_LIGHTING);
     glEnable(GL_DEPTH_TEST);
-    glEnable (GL_TEXTURE_2D );
+    glEnable(GL_TEXTURE_2D);
 
     glFrontFace(GL_CW);
     glCullFace(GL_FRONT);
     glEnable(GL_CULL_FACE);
 
     glDepthFunc(GL_LEQUAL);
-    glDepthRange(50,-50);  
+    glDepthRange(50, -50);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     glDepthMask(GL_TRUE);
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glColor3f(0.0, 0.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);  
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 }
 
-void keyboard(unsigned char key, int x, int y) {
-    switch(key) {
-        case 'a':
-           cameraX -= 1;
-            break;
-        case 'd':
-            cameraX += 1;
-            break;
-        case 'w':
-            cameraY += 1;
-            break;
-        case 's':
-            cameraY -= 1;
-            break;
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'a':
+        cameraX -= 1;
+        break;
+    case 'd':
+        cameraX += 1;
+        break;
+    case 'w':
+        cameraY += 1;
+        break;
+    case 's':
+        cameraY -= 1;
+        break;
     }
     glutPostRedisplay();
 }
 
-void mouse(int button, int state, int x, int y) {
-    if(button == 3)
+void mouse(int button, int state, int x, int y)
+{
+    if (button == 3)
         cameraZ -= 1;
-    if(button == 4)
+    if (button == 4)
         cameraZ += 1;
     glutPostRedisplay();
 }
 
-void myReshape(int w, int h) {
-    float aspectRatio = (float)w/(float)h;
+void myReshape(int w, int h)
+{
+    float aspectRatio = (float)w / (float)h;
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    gluPerspective(45,aspectRatio,-10,10);
+    gluPerspective(45, aspectRatio, -10, 10);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
-void timer(int value) {
+void timer(int value)
+{
     alpha += step_alpha;
     theta += 1.0;
     gama += step_gama;
-    
-    if(alpha > 18)
+
+    if (alpha > 18)
         omega -= 0.1;
 
-    if(theta >= 360)
-        theta=0.0;
+    if (theta >= 360)
+        theta = 0.0;
 
-    if(alpha > 50){
-        alpha=0.0;
+    if (alpha > 50)
+    {
+        alpha = 0.0;
         omega = 0;
     }
 
-    if(fabs(objetos[0]->posZ - alpha) < 2*raio) {
-        step_alpha = 0;
+    if (fabs(objetos[0]->posZ - alpha) < 2 * raio)
+    {
+        step_alpha = -0.001;
         step_gama = 0.2;
     }
     glutPostRedisplay();
     glutTimerFunc(10, timer, 0);
 }
 
-int main(int argc, char **argv){
-    if(argc != 3){
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
         cout << "\nUso: " << argv[0] << " <arquivo.obj> <arquivo.obj>" << endl;
         return 1;
     }
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutCreateWindow(argv[0]);
-    //glutInitWindowSize(720, 576);
-    glutFullScreen();
+    glutInitWindowSize(1000, 576);
+    // glutFullScreen();
     init();
     glutDisplayFunc(display);
 
@@ -170,13 +194,21 @@ int main(int argc, char **argv){
     glutReshapeFunc(myReshape);
     glutTimerFunc(10, timer, 0);
 
-    Objeto *obj1 = new Objeto(0, 0, 20);
+    Objeto *obj1 = new Objeto(0, 0, 30);
     obj1->carregarObjeto(argv[1]);
     objetos.push_back(obj1);
 
     Objeto *obj2 = new Objeto(0, 0, 0);
     obj2->carregarObjeto(argv[2]);
     objetos.push_back(obj2);
+
+    Objeto *obj3 = new Objeto(2, 0, 0);
+    obj3->carregarObjeto(argv[2]);
+    objetos.push_back(obj3);
+
+    Objeto *obj4 = new Objeto(-2, 0, 0);
+    obj4->carregarObjeto(argv[2]);
+    objetos.push_back(obj4);
 
     glutMainLoop();
 
